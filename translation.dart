2d72079a36a2adf9f06a85574d5f126a0412d6e1
AhmedAtef <90651336+AhmedAtef283@users.noreply.github.com>
2022-05-03 00:39:24 +0200

    Create translation.dart

---
+++ translation.dart
@@ -0,0 +1,39 @@
+import 'package:get/get.dart';
+
+class trans extends Translations{
+  @override
+  Map<String, Map<String, String>> get keys =>{
+ 'Ar':{
+
+   'Login':'تسجيل الدخول' ,
+   'Login Screen' : 'صفحة تسجيل الدخول',
+   'Email Address' : 'البريد الالكتروني',
+   'Password' : 'كلمة المرور',
+   'Don\'t Have Account ?' : 'لا يوجد لديك حساب ؟',
+    'Register Now' :'سجل الان',
+   'LOGIN':'تسجيل الدخول'
+ } ,
+
+    'ENG':{
+
+      'تسجيل الدخول':'Login',
+      'صفحة تسجيل الدخول' : 'Login Screen',
+      'البريد الالكتروني' : 'Email Address',
+      'كلمة المرور' : 'Password',
+      'لا يوجد لديك حساب ؟' : 'Don\'t Have Account ?',
+      'سجل الان':'Register Now',
+      'تسجيل الدخول':'Login',
+
+    }
+
+
+
+
+
+
+
+
+
+};
+
+}
